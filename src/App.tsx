04d15c7d0f6fca442cb1ba9bb336035971a8d469
Sofia <sofiamotetta01@gmail.com>
2026-04-28 16:24:53 +0100

    Update heading for first Vercel deploy

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Ciao! Sono Sofia</h1>
+          <h1>Ciao! Sono culetto</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
